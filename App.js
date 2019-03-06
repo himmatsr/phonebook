0e965fa09a78326548e6985d47c414bc612fd7cf
himmat singh <42744450+himmatsr@users.noreply.github.com>
2019-03-06 23:19:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Display from './contacts';
+let contacts=[];
+
+class App extends Component {
+constructor(){
+  super();
+  this.state={
+  name:'',
+  phone:'',
+  save:0,
+  filteredcontact:contacts
+  
+}
+}
+
+ onPhoneChange=(event)=>{
+    this.setState({phone:event.target.value})
+    //console.log(event.target.value);
+  }
+  onSubmitData=()=>{
+  this.setState({save:1});
+  contacts.push({name:this.state.name,phone:this.state.phone})
+ this.setState({filteredcontact:contacts});
+ //console.log(this.state);
+ //console.log(contacts);
+}
+  onNameChange=(event)=>{
+    this.setState({name:event.target.value})
+    //console.log(event.target.value);
+  }
+  onDelete=(index)=>{
+    console.log("hello");
+    contacts.splice(index,1);
+    this.setState({filteredcontact:contacts});
+
+  }
+  render() {
+    return (
+      <div className="him">
+      <input type="button" id="01" value="Add contact" onClick={()=>{ this.setState({save:0})}} />
+      {
+        (this.state.save==1)?
+      <Display contact={this.state.filteredcontact} ondelete={this.onDelete}/>:
+      <div>
+      <p>Name:</p>
+      <input id="02" onChange={this.onNameChange}/>
+      <p>phone:</p>
+      <input id="03" onChange={this.onPhoneChange}/>
+      <p>Name:{`${this.state.name}`}</p>
+      <p>Phone:{`${this.state.phone}`}</p> 
+      <input id="04" type="submit" value="Save" onClick={this.onSubmitData} />
+      </div>
+     }
+     </div>
+    ); 
+}
+}
+
+export default App;
